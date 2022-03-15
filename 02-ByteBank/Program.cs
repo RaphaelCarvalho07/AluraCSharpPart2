f0d30ca0db10626788aebdc58472678510683e28
Raphael Carvalho <raphilskerj@gmail.com>
2022-03-15 18:32:52 -0300

    Aula 10.Exercício Tipos - 02.Tipos - Alura CSharp Parte 2

---
+++ 02-ByteBank/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_ByteBank
+{
+    internal class Program
+    {
+        static void Main( string[ ] args )
+        {
+            ContaCorrente primeiraContaCorrente = new ContaCorrente();
+            primeiraContaCorrente.saldo = 200;
+            Console.WriteLine(primeiraContaCorrente.saldo);
+
+            primeiraContaCorrente.saldo += 100;
+            Console.WriteLine( primeiraContaCorrente.saldo );
+
+            ContaCorrente segundaContaCorrente = new ContaCorrente();
+            segundaContaCorrente.saldo = 50;
+            
+            Console.WriteLine($"Primeira Conta tem: {primeiraContaCorrente.saldo}");
+            Console.WriteLine($"Segunda Conta tem: {segundaContaCorrente.saldo}");
+        }
+    }
+}
