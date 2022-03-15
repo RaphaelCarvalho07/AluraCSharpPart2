a8c87cc07ee430b4d63f8c19bd3bbed3ff771a38
Raphael Carvalho <raphilskerj@gmail.com>
2022-03-15 20:01:04 -0300

    Aula 09.Exercício Criando Métodos - 03.Comportamentos de Classes - Alura CSharp Parte 2

---
+++ 04-ByteBank/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04_ByteBank
+{
+    internal class Program
+    {
+        static void Main( string[ ] args )
+        {
+            ContaCorrente contaDoRapha = new ContaCorrente();
+            ContaCorrente contaDaBina = new ContaCorrente();
+
+            contaDoRapha.titular = "Raphael";
+            contaDaBina.titular = "Bina";
+            Console.WriteLine( $"Saldo atual da {contaDaBina.titular} é {contaDaBina.saldo }" );
+            Console.WriteLine( $"Saldo atual da {contaDaBina.titular} é {contaDaBina.saldo }" );
+            
+            contaDoRapha.Depositar( 500 );
+            contaDaBina.Sacar( 50 );
+            Console.WriteLine( $"Saldo atual do {contaDoRapha.titular} é {contaDoRapha.saldo }" );
+            Console.WriteLine( $"Saldo atual da {contaDaBina.titular} é {contaDaBina.saldo }" );
+            
+            contaDoRapha.Transferir(150, contaDaBina);
+            Console.WriteLine( $"Saldo atual do {contaDoRapha.titular} é {contaDoRapha.saldo }" );
+            Console.WriteLine( $"Saldo atual da {contaDaBina.titular} é {contaDaBina.saldo }" );
+        }
+    }
+}
