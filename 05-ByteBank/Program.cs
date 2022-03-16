d86849c6242cdd42884321204e240c95fb469a87
Raphael Carvalho <raphilskerj@gmail.com>
2022-03-16 08:59:35 -0300

    Aula 10.Exercício Referência - 04.Composição Namespace e Null - Alura CSharp Parte 2

---
+++ 05-ByteBank/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05_ByteBank
+{
+    internal class Program
+    {
+        static void Main( string[ ] args )
+        {
+            ContaCorrente contaDaBina = new ContaCorrente();
+            contaDaBina.titular = new Cliente();
+
+            contaDaBina.titular.name = "Bina Carvalho";
+            contaDaBina.titular.cpf = "485.587.936.17";
+            contaDaBina.titular.profissao = "Advogata";
+            contaDaBina.agencia = 487;
+            contaDaBina.numero = 487121;
+            contaDaBina.Depositar( 1000 );
+
+            Console.WriteLine( "==============Lista de Clientes VIP===================" );
+            Console.WriteLine( $"Titular: {contaDaBina.titular.name}" );
+            Console.WriteLine( $"CPF: {contaDaBina.titular.cpf}" );
+            Console.WriteLine( $"Profissão: {contaDaBina.titular.profissao}" );
+            Console.WriteLine( $"Agência: {contaDaBina.agencia}" );
+            Console.WriteLine( $"Número: {contaDaBina.numero}" );
+            Console.WriteLine( $"Saldo: {contaDaBina.saldo}" );
+        }
+    }
+}
